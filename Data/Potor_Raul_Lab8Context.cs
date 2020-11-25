844ab54499fb810b2b03e21ed337023e3d23d124
RaulPotor <44375993+RaulPotor@users.noreply.github.com>
2020-11-25 16:52:22 +0200

    Add project files.

---
+++ Data/Potor_Raul_Lab8Context.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Potor_Raul_Lab8.Models;
+
+namespace Potor_Raul_Lab8.Data
+{
+    public class Potor_Raul_Lab8Context : DbContext
+    {
+        public Potor_Raul_Lab8Context (DbContextOptions<Potor_Raul_Lab8Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Potor_Raul_Lab8.Models.Book> Book { get; set; }
+
+        public DbSet<Potor_Raul_Lab8.Models.Publisher> Publisher { get; set; }
+
+        public DbSet<Potor_Raul_Lab8.Models.Category> Category { get; set; }
+    }
+}
